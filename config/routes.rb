51396fa225407fdc7fdf4e533156c6e7ec77720f
ninebelowzero <richardjohnsilk@gmail.com>
2015-11-02 15:19:50 +0000

    Added forms layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :articles
-  devise_for :users
+  devise_for :users, controllers: {sessions: "sessions", registrations: "registrations" }
   root "articles#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
