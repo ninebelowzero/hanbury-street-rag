4801eeeb0166d8674b01e55189778ca015bb9df2
ninebelowzero <richardjohnsilk@gmail.com>
2015-11-03 19:46:16 +0000

    Admin can now remove topics. Can't add them.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,14 +1,28 @@
 class TopicsController < ApplicationController
 
-	before_action :set_topic, only: [:show, :edit, :update, :destroy]
+	before_action :set_topic, only: [:destroy]
+
+  def index
+
+  end
 
 	def show
 	end
+
+  def destroy
+    @topic.destroy
+    redirect_to topics_path, notice: "Topic deleted."
+  end
 
 	private
 
 		def set_topic
 			@topic = Topic.find(params[:id])
 		end
+
+    def topic_params
+      params.require(:topic).permit(:name)
+    end
+
 
 end
