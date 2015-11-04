e52989a928b9ff7c596c39e5890d9001e8f553ee
ninebelowzero <richardjohnsilk@gmail.com>
2015-11-04 22:13:52 +0000

    Tried to fix comments bug. Getting stuck.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
 	protected
 
 		def configure_permitted_parameters
+
 			devise_parameter_sanitizer.for(:sign_up) do |u|
 				u.permit(:name, :username, :email, :password, :password_confirmation, :remember_me)
 			end
